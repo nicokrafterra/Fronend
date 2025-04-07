71d9c6c4dec915d082c3bb29ed271af6df2877a2
nicokrafterra <gamerjuan442@gmail.com>
2025-04-06 21:00:44 -0500

    aaaa

--- reservasvue/vite.config.js
+++ reservasvue/vite.config.js
@@ -4,13 +4,14 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
   ],
   server: {
+    host: '0.0.0.0',
+    port: 5173,
     cors: true,
     proxy: {
       '/api': 'http://localhost:8000',
